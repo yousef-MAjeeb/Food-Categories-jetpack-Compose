c2964fc54f300cc4377b395a70b7227e5ea2a3ca
idiot-Dev <3amkhero@gmail.com>
2026-04-24 01:20:18 +0300

    fix UI

--- app/src/main/java/com/ajeeb/foodcategorieswithcompose/ui/components/CategoryInfoCard.kt
+++ app/src/main/java/com/ajeeb/foodcategorieswithcompose/ui/components/CategoryInfoCard.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 
@@ -28,24 +31,34 @@ fun CategoryInfoCard(image: String, name: String, description: String){
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface,
         ),
-        border = BorderStroke(1.dp, Color.Black)
+        border = BorderStroke(1.dp, Color.Black),
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp)
     ) {
         Row(
-            modifier = Modifier.fillMaxSize().padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             AsyncImage(
                 model = image,
-                contentDescription = name
+                contentDescription = name,
+                modifier = Modifier.size(80.dp)
             )
             Column(
                 modifier = Modifier,
                 verticalArrangement = Arrangement.SpaceAround,
                 horizontalAlignment = Alignment.Start
             ) {
-                Text(text = name)
-                Text(text = description)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = description,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
     }
